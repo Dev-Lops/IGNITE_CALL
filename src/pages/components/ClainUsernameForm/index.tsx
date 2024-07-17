6d5c995a7420c9beb411a223225503ad79391b80
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-07-16 20:36:48 -0400

    ✨ update project from new github

---
+++ src/pages/components/ClainUsernameForm/index.tsx
@@ -0,0 +1,61 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Text, TextInput } from '@ignite-ui/react'
+import { useRouter } from 'next/router'
+import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Form, FormAnotation } from './styles'
+
+const clainUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O usuário pode ter apenas letras e hifens.',
+    })
+    .transform((username) => username.toLowerCase()),
+})
+
+type ClainUsernameFormData = z.infer<typeof clainUsernameFormSchema>
+
+export function ClainUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ClainUsernameFormData>({
+    resolver: zodResolver(clainUsernameFormSchema),
+  })
+
+  const router = useRouter()
+
+  async function handleClaiinUsername(data: ClainUsernameFormData) {
+    const { username } = data
+
+    await router.push(`/register?username=${username}`)
+  }
+
+  return (
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaiinUsername)}>
+        <TextInput
+          size="sm"
+          prefix="iginite.com/"
+          placeholder="seu-usuário"
+          {...register('username')}
+        />
+        <Button size="sm" type="submit" disabled={isSubmitting}>
+          Reservar
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnotation>
+        <Text size="sm">
+          {errors.username
+            ? errors.username.message
+            : 'Digite o nome do usuário desejado.'}
+        </Text>
+      </FormAnotation>
+    </>
+  )
+}
