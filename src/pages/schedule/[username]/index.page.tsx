89c7abea1ede2fbc40c7835f4d5971847aa3f7e4
Anderson Lopes <developes.anderson@gmail.com>
2024-07-24 15:36:01 -0400

    ✨ Página de agendamentos

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,49 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { Container, UserHead } from './styles'
+import type { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '@/lib/prisma'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHead>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHead>
+    </Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+  if (!user) {
+    return { notFound: true }
+  }
+  return {
+    props: {
+      user: { name: user.name, bio: user.bio, avatarUrl: user.avatar_url },
+    },
+    revalidate: 60 * 60 * 24, // 1day
+  }
+}
